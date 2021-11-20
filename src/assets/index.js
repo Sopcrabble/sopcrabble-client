0bcd5812c58ec7b9367c6a20a94ca20b9cea9dd6
100Gyeon <bjy9966@naver.com>
2021-11-21 05:26:50 +0900

    chore: icons에서 images로 수정

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,5 +3,5 @@ export { default as ic_arrow_forward } from './icons/ic_arrow_forward.svg';
 export { default as ic_heart_default } from './icons/ic_heart_default.svg';
 export { default as ic_heart_selected } from './icons/ic_heart_selected.svg';
 export { default as ic_select } from './icons/ic_select.svg';
-export { default as img_puzzle_green } from './icons/img_puzzle_green.png';
-export { default as img_puzzle_pink } from './icons/img_puzzle_pink.png';
+export { default as img_puzzle_green } from './images/img_puzzle_green.png';
+export { default as img_puzzle_pink } from './images/img_puzzle_pink.png';
