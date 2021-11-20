66b211052dcdae565ef1d22aac3b00cbea1b0b71
Bebop <tjr50999@naver.com>
2021-11-21 07:14:06 +0900

    feat: home, registration 컴포넌트 구현

--- src/components/common/GlobalStyle.js
+++ src/components/common/GlobalStyle.js
@@ -21,6 +21,10 @@ const GlobalStyle = createGlobalStyle`
     }
   }
 
+  body::-webkit-scrollbar {
+      display: none; /* Chrome, Safari, Opera*/
+  }
+
   html {
     font-size: 62.5%;
   }
@@ -46,6 +50,38 @@ const GlobalStyle = createGlobalStyle`
 
   body, button {
     font-family: Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, 'Helvetica Neue', 'Segoe UI', 'Apple SD Gothic Neo', 'Noto Sans KR', 'Malgun Gothic', sans-serif;
+  }
+
+  @keyframes fadeIn {
+    from {
+      opacity: 0;
+      transform: translateY(8rem);
+    }
+
+    to {
+      opacity: 3;
+      transform: none;
+    }
+  }
+  
+  .logo {
+    position: absolute;
+    top: 5%;
+    left: 10%;
+  }
+
+  .buttons {
+    position: absolute;
+    bottom: 10%;
+    right: 10%;
+  }
+
+  .fiction {
+    position: absolute;
+    z-index: 10;
+    opacity: 0;
+    width: 50%;
+    height: 50vh;
   }
 `;
 
--- src/pages/Registration.jsx
+++ src/pages/Registration.jsx
@@ -1,7 +1,110 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styled from 'styled-components';
 
 const Registration = () => {
-  return <div></div>;
+  const [values, setValues] = useState({ title: '', wordNum: '' });
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setValues({ ...values, [name]: value });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  console.log(values.wordNum);
+
+  return (
+    <StyledRegistration>
+      <div> 솝조어를 만들고 싶나요? 만들고 싶은 솝조어를 의뢰해보세요. </div>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="title">의뢰명</label>
+          <input
+            id="title"
+            value={values.title}
+            name="title"
+            onChange={handleChange}
+            placeholder="의뢰명을 입력해주세요."
+          />
+        </div>
+        <div>
+          <label htmlFor="num" className="second">
+            글자수
+          </label>
+          <input
+            id="num"
+            value={values.wordNum}
+            name="wordNum"
+            onChange={handleChange}
+            placeholder="글자 수를 2~6자로 입력해주세요."
+          />
+        </div>
+        <button type="submit">의뢰</button>
+      </form>
+    </StyledRegistration>
+  );
 };
 
 export default Registration;
+
+const StyledRegistration = styled.div`
+  width: 100%;
+  height: 100vh;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  animation: fadeIn 1.5s ease-in-out;
+
+  & > div:first-child {
+    font-weight: bold;
+    font-size: 3.5rem;
+    line-height: 4.2rem;
+
+    margin-bottom: 9.6rem;
+  }
+
+  & > form > div:first-child {
+    margin-bottom: 2.5rem;
+  }
+
+  & > form > div > label:first-child {
+    font-weight: bold;
+    font-size: 2.8rem;
+    line-height: 3.4rem;
+
+    margin-right: 2.5rem;
+  }
+
+  & > form > div > input {
+    width: 99rem;
+    height: 7.5rem;
+    padding-left: 1rem;
+    border: 0.8rem solid #000000;
+    border-radius: 1rem;
+
+    font-size: 2.2rem;
+    font-family: Pretendard;
+  }
+
+  & > form > button {
+    position: relative;
+    right: -89%;
+
+    width: 12.1rem;
+    height: 4.8rem;
+    margin-top: 5.6rem;
+    border-radius: 1.5rem;
+
+    background-color: #000000;
+    color: white;
+
+    font-weight: bold;
+    font-size: 2.4rem;
+    line-height: 2.9rem;
+  }
+`;
