0841306b164489f2ae4c5088f867687e352e79a0
100Gyeon <bjy9966@naver.com>
2021-11-21 07:01:42 +0900

    feat: favorite 페이지 구조화 및 스타일링

--- src/pages/Favorite.jsx
+++ src/pages/Favorite.jsx
@@ -1,7 +1,47 @@
 import React from 'react';
+import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
+import { ic_arrow_back, img_favorite } from 'assets';
 
 const Favorite = () => {
-  return <div></div>;
+  const navigate = useNavigate();
+  const handleClick = () => {
+    navigate('/home');
+  };
+  return (
+    <StyledFavorite>
+      <StyledHeader>
+        <button onClick={handleClick}>
+          <img src={ic_arrow_back} />
+        </button>
+        <div>솝조어</div>
+      </StyledHeader>
+      <img src={img_favorite} />
+    </StyledFavorite>
+  );
 };
 
 export default Favorite;
+
+const StyledFavorite = styled.div`
+  & > img {
+    margin-top: 9.2rem;
+    margin-bottom: 11.6rem;
+  }
+`;
+
+const StyledHeader = styled.div`
+  display: flex;
+  align-items: center;
+  margin-top: 1.7rem;
+  & > button {
+    padding: 0;
+    background: transparent;
+    margin-right: 5.4rem;
+  }
+  & > div {
+    line-height: 4.2rem;
+    font-size: 3.5rem;
+    font-weight: 700;
+  }
+`;
